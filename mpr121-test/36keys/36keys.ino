bd6fdaf8311ccdf37e900e3e126e8bf852cec399
Ryoga-exe <git@ryoga.dev>
2024-06-25 04:14:49 +0900

    refactor

--- mpr121-test/36keys/36keys.ino
+++ mpr121-test/36keys/36keys.ino
@@ -1,9 +1,10 @@
 #include <Wire.h>
 #include "Adafruit_MPR121.h"
 
-Adafruit_MPR121 cap0 = Adafruit_MPR121();
-Adafruit_MPR121 cap1 = Adafruit_MPR121();
-Adafruit_MPR121 cap2 = Adafruit_MPR121();
+constexpr uint8_t SensorNum = 12;
+constexpr uint8_t CapsCount = 3;
+constexpr uint8_t I2cAddrs[] = { 0x5C, 0x5B, 0x5A };
+Adafruit_MPR121 caps[CapsCount];
 
 void setup() {
   Serial.begin(9600);
@@ -14,55 +15,29 @@ void setup() {
   }
   Serial.println("Serial is ready!");
 
-  if (!cap0.begin(0x5A)) {
-    Serial.println("MPR121 (0x5A) not found");
-    while (true) {}
+  for (uint8_t i = 0; i < CapsCount; i++) {
+    caps[i] = Adafruit_MPR121();
+    if (!caps[i].begin(I2cAddrs[i])) {
+      Serial.print("Caps ");
+      Serial.print(i);
+      Serial.println(" not found.");
+      while (true) {}
+    }
   }
-  Serial.println("MPR121 (0x5A) is ready!");
 
-  if (!cap1.begin(0x5B)) {
-    Serial.println("MPR121 (0x5B) not found");
-    while (true) {}
-  }
-  Serial.println("MPR121 (0x5B) is ready!");
-
-  if (!cap2.begin(0x5C)) {
-    Serial.println("MPR121 (0x5C) not found");
-    while (true) {}
-  }
-  Serial.println("MPR121 (0x5C) is ready!");
+  Serial.println("ready!");
 }
 
 void loop() {
-  auto touched = cap2.touched();
-
-  for (uint8_t i = 0; i < 12; i++) {
-    if (touched & (1 << i)) {
-      Serial.print("#");
-    } else {
-      Serial.print(".");
+  for (uint8_t i = 0; i < CapsCount; i++) {
+    const auto touched = caps[i].touched();
+    for (uint8_t t = 0; t < SensorNum; t++) {
+      if (touched & (1 << t)) {
+        Serial.print("#");
+      } else {
+        Serial.print(".");
+      }
     }
   }
-
-  touched = cap1.touched();
-
-  for (uint8_t i = 0; i < 12; i++) {
-    if (touched & (1 << i)) {
-      Serial.print("#");
-    } else {
-      Serial.print(".");
-    }
-  }
-
-  touched = cap0.touched();
-
-  for (uint8_t i = 0; i < 12; i++) {
-    if (touched & (1 << i)) {
-      Serial.print("#");
-    } else {
-      Serial.print(".");
-    }
-  }
-
   Serial.println("");
 }
